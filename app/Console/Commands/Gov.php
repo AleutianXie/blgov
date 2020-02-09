b5a5b057f403326ffde582541a6391b314233dfe
Aleutian Xie <aleutian.xie@cicisoft.cn>
2020-02-09 13:22:40 +0800

    add other gov api

--- app/Console/Commands/Gov.php
+++ app/Console/Commands/Gov.php
@@ -91,7 +91,6 @@ class Gov extends Command
         }
 
         // 假期外出情况
-        // 员工性别男
         $employee_outing_count = DB::table('employeeInfoTable')
             ->join('enterpriseInfoTable', 'employeeInfoTable.EnterpriseID', '=', 'enterpriseInfoTable.EnterpriseID')
             ->select('TownID', 'OutgoingSituation', DB::raw('count(*) as total'))
@@ -131,6 +130,69 @@ class Gov extends Command
         foreach ($back_total as $key => $total) {
             Redis::zAdd('blgov:summary:employee_back_date:count:' . $yz_townID, $total, $key);
         }
+
+        // 接触情况
+        $employee_contact_situation_count = DB::table('employeeInfoTable')
+            ->join('enterpriseInfoTable', 'employeeInfoTable.EnterpriseID', '=', 'enterpriseInfoTable.EnterpriseID')
+            ->select('TownID', 'ContactSituation', DB::raw('count(*) as total'))
+            ->groupBy('TownID')
+            ->groupBy('ContactSituation')
+            ->orderBy('TownID', 'asc')
+            ->orderBy('ContactSituation', 'asc')
+            ->get()
+            ->toArray();
+        $contact_situation_total = [0, 0];
+        foreach ($employee_contact_situation_count as $item) {
+            $contact_situation_total[$item->ContactSituation] += $item->total;
+            Redis::hSet('blgov:summary:employee_contact_situation:count:' . $item->TownID, $item->ContactSituation, $item->total);
+        }
+        foreach ($contact_situation_total as $contactSituation => $total) {
+            Redis::hSet('blgov:summary:employee_contact_situation:count:' . $yz_townID, $contactSituation, $total);
+        }
+
+        // 医院观察
+        $employee_is_medical_count = DB::table('employeeInfoTable')
+            ->join('enterpriseInfoTable', 'employeeInfoTable.EnterpriseID', '=', 'enterpriseInfoTable.EnterpriseID')
+            ->select('TownID', 'IsMedicalObservation', DB::raw('count(*) as total'))
+            ->groupBy('TownID')
+            ->groupBy('IsMedicalObservation')
+            ->having('IsMedicalObservation', 1)
+            ->get()
+            ->toArray();
+
+        $is_medical_total = 0;
+        foreach ($employee_is_medical_count as $item) {
+            $is_medical_total += $item->total;
+            Redis::hSet('blgov:summary:employee_is_medical:count', $item->TownID, $item->total);
+        }
+        Redis::hSet('blgov:summary:employee_is_medical:count', $yz_townID, $is_medical_total);
+
+        // 发热情况，不显示正常情况
+        $employee_owner_health_count = DB::table('employeeInfoTable')
+            ->join('enterpriseInfoTable', 'employeeInfoTable.EnterpriseID', '=', 'enterpriseInfoTable.EnterpriseID')
+            ->select('TownID', 'OwnerHealth', DB::raw('count(*) as total'))
+            ->groupBy('TownID')
+            ->groupBy('OwnerHealth')
+            ->having('OwnerHealth', '<>', 0)
+            ->having('OwnerHealth', '<>', 3)
+            ->orderBy('TownID', 'asc')
+            ->orderBy('OwnerHealth', 'asc')
+            ->get()
+            ->toArray();
+
+        $owner_health_total = [0];
+        foreach ($employee_owner_health_count as $item) {
+            if (!isset($owner_health_total[$item->OwnerHealth])) {
+                $owner_health_total[$item->OwnerHealth] = 0;
+            }
+            $owner_health_total[$item->OwnerHealth] += $item->total;
+            Redis::hSet('blgov:summary:employee_owner_health:count:' . $item->TownID, $item->OwnerHealth, $item->total);
+        }
+        foreach ($owner_health_total as $key => $total) {
+            Redis::hSet('blgov:summary:employee_owner_health:count:' . $yz_townID, $key, $total);
+        }
+        var_dump($employee_owner_health_count);
+
         $this->info("Done at: " . date('Y-m-d H:i:s', time()));
     }
 }
--- app/Http/Controllers/GovController.php
+++ app/Http/Controllers/GovController.php
@@ -15,15 +15,15 @@ class GovController extends Controller
         $ret = [];
 
         $enterprise_count = Redis::hGet('blgov:summary:enterprise:count', $townId);
-        $ret['summary'][] = ['label' => '企业数量', 'value' => $enterprise_count ?? 0];
+        $ret['summary'][] = ['label' => '企业数量', 'value' => $enterprise_count ? $enterprise_count : 0];
         $enployee_count = Redis::hGet('blgov:summary:employee:count', $townId);
-        $ret['summary'][] = ['label' => '员工总数', 'value' => $enployee_count ?? 0];
+        $ret['summary'][] = ['label' => '员工总数', 'value' => $enployee_count ? $enployee_count : 0];
         $back_count = Redis::hGet('blgov:summary:employee_back:count', $townId);
-        $ret['summary'][] = ['label' => '在甬员工数', 'value' => $back_count ?? 0];
+        $ret['summary'][] = ['label' => '在甬员工数', 'value' => $back_count ? $back_count : 0];
         $not_back_count = Redis::hGet('blgov:summary:employee_not_back:count', $townId);
-        $ret['summary'][] = ['label' => '非在甬员工数', 'value' => $not_back_count ?? 0];
+        $ret['summary'][] = ['label' => '非在甬员工数', 'value' => $not_back_count ? $not_back_count : 0];
 
-        $gender_count =  Redis::hGetAll('blgov:summary:employee_gender:count:' . $townId);
+        $gender_count = Redis::hGetAll('blgov:summary:employee_gender:count:' . $townId);
         $gender_key = [0 => '男', 1 => '女'];
         if ($gender_count) {
             foreach ($gender_count as $gender => $count) {
@@ -36,7 +36,7 @@ class GovController extends Controller
             ];
         }
 
-        $outing_count =  Redis::hGetAll('blgov:summary:employee_outing:count:' . $townId);
+        $outing_count = Redis::hGetAll('blgov:summary:employee_outing:count:' . $townId);
         $outing_key = [
             0 => '宁波',
             1 => '湖北',
@@ -62,7 +62,8 @@ class GovController extends Controller
     }
 
     // 返甬时间
-    public function back(Request $request) {
+    public function back(Request $request)
+    {
         $townId = $request->get('town', '700000');
         $start = $request->get('start', '2020-02-01');
         $end = $request->get('end', '2020-03-31');
@@ -74,6 +75,60 @@ class GovController extends Controller
         $ret = array_values(Arr::sort($ret, function ($value) {
             return strtotime($value['time']);
         }));
+        return response()->json($ret);
+    }
+
+    // 接触情况
+    public function touch(Request $request)
+    {
+        $townId = $request->get('town', '700000');
+        $ret = [];
+
+        $contact_situation_count = Redis::hGetAll('blgov:summary:employee_contact_situation:count:' . $townId);
+        $contact_situation_key = [0 => '无接触', 1 => '湖北接触'];
+        if ($contact_situation_count) {
+            foreach ($contact_situation_count as $contact_situation => $count) {
+                $ret[] = ['value' => $count, 'key' => $contact_situation_key[$contact_situation]];
+            }
+        } else {
+            $ret = [
+                ['value' => 0, 'key' => '无接触'],
+                ['value' => 0, 'key' => '湖北接触']
+            ];
+        }
+
+        return response()->json($ret);
+    }
+
+    // 医学观察
+    public function medical(Request $request)
+    {
+        $townId = $request->get('town', '700000');
+        $ret = [];
+
+        $is_medical_count = Redis::hGet('blgov:summary:employee_is_medical:count', $townId);
+        $ret[] = ['label' => '医学观察人数', 'value' => $is_medical_count ? $is_medical_count : 0];
+        // 发热情况
+        $owner_health_count = Redis::hGetAll('blgov:summary:employee_owner_health:count:' . $townId);
+        $owner_health_key = [
+            0 => '正常人数',
+            1 => '发热人数',
+            2 => '咳嗽人数',
+            3 => '发热并咳嗽人数',
+            4 => '其它症状人数'
+        ];
+        if ($owner_health_count) {
+            foreach ($owner_health_count as $owner_health => $count) {
+                $ret[] = ['value' => $count, 'key' => $owner_health_key[$owner_health]];
+            }
+        } else {
+            $ret['out'] = [
+                ['value' => 0, 'key' => '发热人数'],
+                ['value' => 0, 'key' => '咳嗽人数'],
+                ['value' => 0, 'key' => '其它症状人数']
+            ];
+        }
+
         return response()->json($ret);
     }
 }
