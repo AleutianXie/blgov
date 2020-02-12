8f33603a75001668a3d6eaf49508e9cc1e6a47af
huyouheng <876697515@qq.com>
2020-02-11 22:20:58 -0500

    fixed company update validation

---
+++ app/Http/Requests/CompanyRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CompanyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'OrganizationCode'  => 'required|max:100',
+            'TownID'            => 'required',
+            'Address'           => 'nullable|max:500',
+            'Contacts'          => 'required|max:50',
+            'PhoneNumber'       => 'nullable|max:50',
+            'EnterpriseScale'   => 'required',
+            'IndustryTableID'   => 'required',
+            'Industry'          => 'nullable|max:100',
+            'EmployeesNumber'   => 'nullable',
+            'BackEmpNumber'     => 'nullable',
+            'StartDate'         => 'nullable|date',
+            'ProductingPlan'    => 'nullable',
+
+        ];
+    }
+
+    public function messages()
+{
+    return [
+        'OrganizationCode.required' => '组织机构代码不能为空!',
+        'StartDate.date'            => '开工时间错误',
+        'Contacts.required'         => '联系人不能为空',
+    ];
+}
+
+}
